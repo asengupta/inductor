a9f31a361f7f6b503abb864780ae2decfb24fce3
avishek-sen-gupta <asgupta@thoughtworks.com>
2025-05-11 14:11:04 +0530

    Renames and ability to export to Mermaid

--- src/inductor_main.py
+++ src/inductor_main.py
@@ -1,9 +1,11 @@
 import asyncio
 from graph.graph_builder import make_graph, start_task_graph, mcp_client
 
-async def refactored_hypo():
+
+async def run_inductor():
     async with make_graph(mcp_client) as graph:
         await start_task_graph("Tell me about this codebase", graph)
 
+
 if __name__ == "__main__":
-    asyncio.run(refactored_hypo())
+    asyncio.run(run_inductor())
