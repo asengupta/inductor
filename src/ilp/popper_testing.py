1615e6cb59e664c1aad3779fbcc50fe32c8ecb01
avishek-sen-gupta <asgupta@thoughtworks.com>
2025-05-17 13:06:39 +0530

    Popper running from inside Python too

--- src/ilp/popper_testing.py
+++ src/ilp/popper_testing.py
@@ -1,7 +1,7 @@
-from popper.util import Settings, print_prog_score
+from popper.util import Settings
 from popper.loop import learn_solution
 
-settings = Settings(kbpath='input_dir')
+settings = Settings(kbpath='ilp')
 prog, score, stats = learn_solution(settings)
 if prog != None:
-    print_prog_score(prog, score)
+    settings.print_prog_score(prog, score)
